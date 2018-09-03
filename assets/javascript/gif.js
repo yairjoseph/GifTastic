19e9c9113d8c9c354d0854a9dfe5c73850a50682
YJ <yair.yoseph@gmail.com>
2018-09-02 19:45:03 -0700

    great progress

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -2,48 +2,75 @@
 // $( document ).ready(function() {
 //api url and key
 var topics = ["tesla", "pagani", "koenigsegg", "bmw", "apollo automobil", "mercedes-benz"];
-var apiKey = "zAEBPU2C4G4HZnkU4vAKHzuzQg0wS2UZ";
 
-// var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + userInput "&api_key=" + apiKey +"&limit=5";    
-// var userInput = ;
-// var imgUrl + userInput
+function runApp() {
+    var userInput = $(this).attr("data-topics");
+    var apiKey = "zAEBPU2C4G4HZnkU4vAKHzuzQg0wS2UZ";
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + userInput +"&api_key=" + apiKey + "&limit=5";
+    // https://api.giphy.com/v1/gifs/search?q=&api_key=zAEBPU2C4G4HZnkU4vAKHzuzQg0wS2UZ&limit=5
+    // // ajax get call to request the query url
+    
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+        // After the data from the AJAX request comes back
+        .then(function (response) {
+            var searchDiv = $("<div class='searchDiv'>");
+            var rating = response.data.rating;
+            var pOne = $("<p>").text("Rating: " + rating);
+            searchDiv.append(pOne);
+            var imageUrl = response.data.images;
 
-// ajax get call to request the query url
-// $.ajax({
-//     url: queryUrl,
-//     method: "GET"
-// })
-for (let i = 0; i < topics.length; i++) {
-    // var imgUrl = response.data[i].images
-    var topicBtn = $("<button>").attr("data-topics", topics[i]);
-    // var topicImg = $("<img>");
-    $("#topicsBtn").append(topicBtn);
-    $(topicBtn).text(topics[i]);
-    // topicBtn.append(topicImg);
-    // topicImg.attr("src", imgUrl)
+            // Creating and storing an image tag
+            var image = $("<img>");
 
+            // Setting the catImage src attribute to imageUrl
+            image.attr("src", imageUrl);
+            image.attr("alt", "image");
+            searchDiv.append(image);
+
+
+            // Prepending the catImage to the images div
+            $("#imageReady").prepend(searchDiv);
+
+
+
+        });
 }
+
+function buttonMaker() {
+    $("#topicsBtn").empty()
+    for (let i = 0; i < topics.length; i++) {
+        var topicBtn = $("<button>").attr("data-topics", topics[i]);
+        $("#topicsBtn").append(topicBtn);
+        $(topicBtn).text(topics[i]);
+
+    }
+}
+
 
 $("#search1").on("click", function (e) {
     e.preventDefault();
     var userInput = $("#userInput").val();
     console.log(userInput)
-    var inputArray = [];
-    inputArray.push(userInput);
-    console.log(inputArray);
-    var completeArray = inputArray.concat(topics);
-    console.log(completeArray)
-    for (let i = 0; i < completeArray.length; i++) {
-        // var imgUrl = response.data[i].images
-        
-        var completeBtn = $("<button>").attr("data-topics", completeArray[i]);
-        // var topicImg = $("<img>");
-        $("#topicsBtn").append(completeBtn);
-        
-        $(completeBtn).text(completeArray[i]);
-    
-}
+   
+    if (userInput === "") {
+        alert("Don't forget to search!")
+    }
+    else {
+        topics.push(userInput);
+        buttonMaker();
+    }
+    // if (!(userInput in topics)){
+    //     alert("No doubles please")
+    // }
+
+
 });
+
+buttonMaker();
+
 //loop run through array
 
 
