85a89d48512c8a0676117c7351c9569f5cda28a7
YJ <yair.yoseph@gmail.com>
2018-09-04 00:45:19 -0700

    getting there

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -6,35 +6,53 @@ var topics = ["tesla", "pagani", "koenigsegg", "bmw", "apollo automobil", "merce
 function runApp() {
     var userInput = $(this).attr("data-topics");
     var apiKey = "zAEBPU2C4G4HZnkU4vAKHzuzQg0wS2UZ";
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + userInput +"&api_key=" + apiKey + "&limit=5";
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=" + apiKey + "&limit=10";
     // https://api.giphy.com/v1/gifs/search?q=&api_key=zAEBPU2C4G4HZnkU4vAKHzuzQg0wS2UZ&limit=5
     // // ajax get call to request the query url
-    
+
     $.ajax({
         url: queryUrl,
         method: "GET"
     })
         // After the data from the AJAX request comes back
         .then(function (response) {
-            var searchDiv = $("<div class='searchDiv'>");
-            var rating = response.data.rating;
-            var pOne = $("<p>").text("Rating: " + rating);
-            searchDiv.append(pOne);
-            var imageUrl = response.data.images;
+            var results = response.data;
+            for (let i = 0; results.length; i++) {
+                
+                var searchDiv = $("<div class='searchDiv'>");
+                var rating = results[i].rating;
+                var pOne = $("<p>").text("Rating: " + rating);
+                //append rating paragraph 
+                searchDiv.append(pOne);
+                //image variable
+                var imageUrl = results[i].images.fixed_height_still;
+                var imageGif = results[i].images.fixed_height_downsampled
 
-            // Creating and storing an image tag
-            var image = $("<img>");
+                // Creating and storing an image tag
+                var image = $("<img>");
 
-            // Setting the catImage src attribute to imageUrl
-            image.attr("src", imageUrl);
-            image.attr("alt", "image");
-            searchDiv.append(image);
+                // Setting the image src attribute to imageUrl
+                image.attr("src", imageUrl);
+                image.attr("alt", "image");
+                searchDiv.append(image);
+                image.attr("data-still", imageUrl)
+                image.attr("data-gif", imageGif)
 
-
-            // Prepending the catImage to the images div
-            $("#imageReady").prepend(searchDiv);
-
-
+                // Prepending the image to the images div
+                $(".data-gif").on("click", function(){
+                    var state = $(this).attr("data-state");
+                    if (state === "still"){
+                        var dataAnimate = $(this).attr("data-animate")
+                        $(this).attr("data-state", "animate")
+                        $(this).attr("src", dataAnimate)
+                    }
+                    else {
+                        $(this).attr("data-state", "still")
+                        $(this).attr("src", $(this).attr("data-still"));
+                    }
+            })
+                $("#imageReady").prepend(searchDiv);
+            }
 
         });
 }
@@ -43,9 +61,9 @@ function buttonMaker() {
     $("#topicsBtn").empty()
     for (let i = 0; i < topics.length; i++) {
         var topicBtn = $("<button>").attr("data-topics", topics[i]);
-        $("#topicsBtn").append(topicBtn);
         $(topicBtn).text(topics[i]);
-
+        topicBtn.addClass("select-btn");
+        $("#topicsBtn").append(topicBtn);
     }
 }
 
@@ -54,7 +72,7 @@ $("#search1").on("click", function (e) {
     e.preventDefault();
     var userInput = $("#userInput").val();
     console.log(userInput)
-   
+
     if (userInput === "") {
         alert("Don't forget to search!")
     }
@@ -68,10 +86,9 @@ $("#search1").on("click", function (e) {
 
 
 });
-
+$(document).on("click", ".select-btn", runApp)
 buttonMaker();
 
-//loop run through array
 
 
 
